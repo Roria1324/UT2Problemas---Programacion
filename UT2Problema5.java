5518474f57c18b72942b380f4d5ed2f7eb188e00
Roria1324 <roriarumanosky@gmail.com>
2024-10-17 17:27:26 +0200

    Update UT2Problema5.java

--- UT2Problema5.java
+++ UT2Problema5.java
@@ -8,7 +8,7 @@ public class UT2Problema5 {
 
         Scanner input = new Scanner(System.in);
 
-        double nota, notaMedia = 0, notaMediaAprobados = 0, notaMediaSuspensos = 0, porcentajeSuspensos = 0, porcentajeAprobados = 0, notaAlta = 0, notaBaja = 10;
+        double nota = 0, notaMedia = 0, notaMediaAprobados = 0, notaMediaSuspensos = 0, porcentajeSuspensos = 0, porcentajeAprobados = 0, notaAlta = 0, notaBaja = 10;
 
         int alumnos = 0, alumnosAprobados = 0, alumnoSuspenso = 0;
 
@@ -16,39 +16,36 @@ public class UT2Problema5 {
         System.out.print("--        ESTADÍSTICAS DE NOTAS          --\n");
         System.out.print("-------------------------------------------\n");
 
-        while (true) {
+        while (nota >= 0) {
             System.out.print("\nIntroduzca una nota (valor negativo para finalizar el programa): ");
             if (input.hasNextDouble()) {
                 nota = input.nextDouble();
                 input.nextLine();
 
                 if (nota > 10) {
-                    System.out.println("Error nota no válida");
+                    System.out.println("Error nota invaálida");
                     continue;
                 }
 
-                if (nota < 0) {
-                    break;
-                }
                 if (nota <= 10 && nota >= 0) {
                     alumnos++;
                     notaMedia = notaMedia + nota;
-                }
-
-                if (nota >= 5) {
-                    alumnosAprobados++;
-                    notaMediaAprobados = notaMediaAprobados + nota;
-                    if (nota > notaAlta) {
-                        notaAlta = nota;
-                    }
-                }
-
-                if (nota < 5 && nota >= 0) {
-                    alumnoSuspenso++;
-                    notaMediaSuspensos = notaMediaSuspensos + nota;
                     if (nota < notaBaja) {
                         notaBaja = nota;
                     }
+                    if (nota > notaAlta) {
+                        notaAlta = nota;
+                    }
+
+                }
+
+                if (nota >= 5 && nota <= 10) {
+                    alumnosAprobados++;
+                    notaMediaAprobados = notaMediaAprobados + nota;
+                }
+                if (nota < 5 && nota >= 0) {
+                    alumnoSuspenso++;
+                    notaMediaSuspensos = notaMediaSuspensos + nota;
                 }
 
             } else {
@@ -56,16 +53,20 @@ public class UT2Problema5 {
                 input.nextLine();
             }
         }
-        porcentajeAprobados = (alumnosAprobados * 100) / alumnos;
-        porcentajeSuspensos = (alumnoSuspenso * 100) / alumnos;
+        if (alumnos == 0) {
+            System.out.println("Error no hay alumnos introducidos");
+        } else {
+            porcentajeAprobados = (alumnosAprobados * 100) / alumnos;
+            porcentajeSuspensos = (alumnoSuspenso * 100) / alumnos;
 
-        System.out.printf("-Se han evaluado un total de %d \n", alumnos);
-        System.out.printf("-La nota media del curso ha sido %.2f\n", notaMedia / alumnos);
-        System.out.printf("-Han aprobado un total de %d , lo que supone un %.2f %% del total\n", alumnosAprobados, porcentajeAprobados);
-        System.out.printf("-La nota media de los alumnos aprobados ha sido %.2f\n", notaMediaAprobados / alumnosAprobados);
-        System.out.printf("-Han suspendido un total de %d, lo que supone un %.2f %% del total\n", alumnoSuspenso, porcentajeSuspensos);
-        System.out.printf("-La nota media de los alumnos suspensos ha sido %.2f\n", notaMediaSuspensos / alumnoSuspenso);
-        System.out.printf("La nota más alta es %.2f\n", notaAlta);
-        System.out.printf("La nota más baja es %.2f\n", notaBaja);
+            System.out.printf("-Se han evaluado un total de %d \n", alumnos);
+            System.out.printf("-La nota media del curso ha sido %.2f\n", notaMedia / alumnos);
+            System.out.printf("-Han aprobado un total de %d, lo que supone un %.2f %% del total\n", alumnosAprobados, porcentajeAprobados);
+            System.out.printf("-La nota media de los alumnos aprobados ha sido %.2f\n", notaMediaAprobados / alumnosAprobados);
+            System.out.printf("-Han suspendido un total de %d, lo que supone un %.2f %% del total\n", alumnoSuspenso, porcentajeSuspensos);
+            System.out.printf("-La nota media de los alumnos suspensos ha sido %.2f\n", notaMediaSuspensos / alumnoSuspenso);
+            System.out.printf("-La nota más alta es %.2f\n", notaAlta);
+            System.out.printf("-La nota más baja es %.2f\n", notaBaja);
+        }
     }
 }
